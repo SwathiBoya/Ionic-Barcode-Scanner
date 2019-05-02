d5683e933adee3253e2f997f727166ebce9d7c12
SwathiBoya <boyaswathi25@gmail.com>
2019-05-02 17:47:02 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,37 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: "AIzaSyAuDmbS85wWzosSpyQx0Vkt0HlTa1v6tgg",
+  authDomain: "notification-8af05.firebaseapp.com",
+  databaseURL: "https://notification-8af05.firebaseio.com",
+  projectId: "notification-8af05",
+  storageBucket: "notification-8af05.appspot.com",
+  messagingSenderId: "779395012762"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+import { Component } from "@angular/core";
+import {
+  BarcodeScannerOptions,
+  BarcodeScanner
+} from "@ionic-native/barcode-scanner/ngx";
+ 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-
+  encodeData: any;
+  scannedData: {};
+  barcodeScannerOptions: BarcodeScannerOptions;
+ 
+  constructor(private barcodeScanner: BarcodeScanner) {
+    this.encodeData = "https://www.FreakyJolly.com";
+    //Options
+    this.barcodeScannerOptions = {
+      showTorchButton: true,
+      showFlipCameraButton: true
+    };
+  }
+ 
+  scanCode() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        alert("Barcode data " + JSON.stringify(barcodeData));
+        this.scannedData = barcodeData;
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+ 
+  encodedText() {
+    this.barcodeScanner
+      .encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodeData)
+      .then(
+        encodedData => {
+          console.log(encodedData);
+          this.encodeData = encodedData;
+        },
+        err => {
+          console.log("Error occured : " + err);
+        }
+      );
+  }
 }
